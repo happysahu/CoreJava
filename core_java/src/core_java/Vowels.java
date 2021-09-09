e68d76c50d18e096af88cc33f19df2c0bf0cdd7c
happysahu <happysahu619@gmail.com>
2021-09-09 14:55:23 +0530

    test.

---
+++ core_java/src/core_java/Vowels.java
@@ -0,0 +1,30 @@
+package core_java;
+
+import java.util.Scanner;
+
+public class Vowels {
+	public static void main(String[] args)
+	{
+		
+		char ch;
+		Scanner ss=new Scanner(System.in);
+		System.out.println("Enter any Character");
+		ch=ss.next().charAt(0);
+		 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) {
+	            System.out.println(ch + " is vowel");
+		 }
+		 else if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+			 System.out.println(ch + " is vowel");
+			 
+		 }
+	            else {
+	            System.out.println(ch + " is consonant");
+	            }
+			ss.close();
+		
+	
+		
+	}
+
+
+}
